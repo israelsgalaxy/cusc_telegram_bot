e5b426b0f804b10bfbcfe61748c00651753a55a4
Israel Galadima <izzygaladima@gmail.com>
2023-03-07 20:44:36 +0100

    Remove name equals main condition

--- app/main.py
+++ app/main.py
@@ -282,6 +282,6 @@ def webhook():
     return "!", 200
 
 
-if __name__ == "__main__":
-  print("Bot is alive")
-  server.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+# if __name__ == "__main__":
+print("Bot is alive")
+server.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
