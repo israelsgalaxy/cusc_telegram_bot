3f513ce991dc6da9afb0b764599a017e6a5f9817
Israel Galadima <izzygaladima@gmail.com>
2023-03-08 13:06:19 +0100

    Use gunicorn

--- app/main.py
+++ app/main.py
@@ -281,8 +281,6 @@ def webhook():
     bot.set_webhook(url=APP_URL + "/" + TOKEN)
     return "!", 200
 
-# if __name__ == "__main__":
-#   server.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
-
-server.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
-print("Bot is alive")
+if __name__ == "__main__":
+  print("Bot is alive")
+  server.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
